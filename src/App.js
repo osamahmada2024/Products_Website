d339b7f9d235045075716001832388762158d757
osamahamada2024 <hmsosamagithub773@gmail.com>
2024-08-09 15:38:10 +0300

    edit on home

--- src/App.js
+++ src/App.js
@@ -9,7 +9,6 @@ function App() {
     <div className="App">
       <Navbar />
       <div className="margin-top container">
-        <Home />
         <Routes>
           <Route path="/" element={<Home />} />
           <Route path="/products" element={<Home />} />
